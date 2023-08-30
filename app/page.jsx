bb17d357934c7e567bb9dba440fc518fbfd21f28
melchanatan <chanatan@outlook.com>
2023-08-30 20:46:38 +0700

    initialize vercel/analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Feed from "@components/Feed"
 import { Router, useRouter } from 'next/navigation'
 import Footer from '@components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 
 const Home = () => {
   
   return (
     <section className="w-full md:px-4 lg:px-8 justify-items-start mt-10">
+      <Analytics />
         <h1 className="head_text max-md px-8 sm:px-6">
             FRA161 lending service
         </h1>
